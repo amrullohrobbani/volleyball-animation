ef400f605cfdb1b0257c01df149b56de790badf8
amrullohrobbani <39686205+amrullohrobbani@users.noreply.github.com>
2024-09-09 05:10:27 +0000

    add electron t the mix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="flex w-full items-center justify-items-center font-[family-name:var(--font-geist-sans)]">
       <main className="grid grid-cols-2 gap-8 w-full items-center sm:items-start">
         <AnimationCard />
-        <AnimationCard />
       </main>
     </div>
   );
